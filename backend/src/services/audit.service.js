af2875119e585568693a387d11bf18839cf4ce44
Mark Bartolome <159106939+sadvoidhours@users.noreply.github.com>
2026-06-16 18:41:13 +0800

    lol

--- backend/src/services/audit.service.js
+++
@@ -1,7 +0,0 @@
-const AuditLog = require('../models/AuditLog');
-
-const logAudit = async ({ actorId, actorRole, action, metadata = {} }) => {
-  return AuditLog.create({ actorId, actorRole, action, metadata });
-};
-
-module.exports = { logAudit };
